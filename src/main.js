3fbc4f350ed492d073ba56b1c09d067227484c33
James Henager <jameschenager@gmail.com>
2020-06-22 15:27:14 -0700

    add user interface logic, add working prototype to count letters and output spans in html
    Co-authored-by: Ben White <bwhite2140@outlook.com>

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import './styles.css';
+import $ from 'jquery';
+import { Entry } from './scripts.js';
+
+
+$(document).ready(function(){
+  $("#journal").submit(function(event){
+    event.preventDefault();
+    const title1Input = $("input#title").val();
+    const body1Input = $("input#body").val();
+
+    let entry1 = new Entry(title1Input, body1Input);
+    let entry1WordCount = entry1.wordCount(body1Input);
+    let entry1LetterCount = entry1.letterCount(body1Input);
+    $(".words").text(entry1WordCount);
+    $(".letters").text(entry1LetterCount);
+    $("#output").show();
+
+  });
+});
--- src/scripts.js
+++ src/scripts.js
@@ -1,20 +1,24 @@
-function Entry (title, body) {
+export function Entry (title, body) {
   this.title = title,
-  this.body = body
+  this.body = body;
 }
 
 Entry.prototype.wordCount = function(body) {
   let wordArray = body.split(" ");
   return wordArray.length;
-}
+};
 
 Entry.prototype.letterCount = function(body) {
   let count = 0;
-  const letters = body.split();
-  letters.forEach(function(letter) {
-    if (letter === /^[A-Za-z]+$/) {
-      count++;
+  body = body.toLowerCase();
+  const characters = body.split('');
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  characters.forEach(function(character) {
+    for (let i = 0; i < alphabet.length; i++) {
+      if (character === alphabet[i]) {
+        count++;
+      }
     }
   });
   return count;
-}
+};
